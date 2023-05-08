2b8a12a2994da84c2f0807694081e0258b8f5df4
Moyasi Ginko <mrsiyam61@gmail.com>
2023-05-09 05:51:08 +0600

    Add luxon in index.js file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+import { DateTime } from './modules/luxon.js';
 import UI from './modules/ui.js';
 import Store from './modules/store.js';
 import pageManager from './modules/pagemanager.js';
@@ -6,3 +7,9 @@ import pageManager from './modules/pagemanager.js';
 new UI();
 new Store();
 new pageManager();
+
+const date = document.querySelector('#date');
+setInterval(() => {
+  const now = DateTime.now();
+  date.textContent = now.toLocaleString(DateTime.DATETIME_MED_WITH_SECONDS);
+}, 1000);
